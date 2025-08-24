831641fbde3f42545388e9fffe520fe1a7e34844
yanalialiuk <ylialuke@gmail.com>
2025-08-24 15:03:10 +0300

    q

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from data_builder import build_dataset
+from data_processing.data_builder import build_dataset
 from fact_check import fact_check
 
 if __name__ == "__main__":
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     # build_dataset()  
     
     print("Проверка факта...")
-    query = "Анкилозавр был хищником"
+    query = "Характерной особенностью анкилозавров  являются костные образования на туловище."
     result = fact_check(query)
 
     print("Результат:")
